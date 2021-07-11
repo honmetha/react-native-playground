41c466f619a03fb4bfaabbcc28adb9e3b3d187c0
Hon <dennis.mejen@gmail.com>
2021-07-11 14:16:58 +0700

    [Added] 17 - Custom Fonts

--- App.js
+++ App.js
@@ -1,7 +1,27 @@
-import { StatusBar } from "expo-status-bar";
 import * as React from "react";
+import * as Font from "expo-font";
+import AppLoading from "expo-app-loading";
+
 import Home from "./screens/home";
 
+const getFonts = () =>
+  Font.loadAsync({
+    "playfairDisplay-regular": require("./assets/fonts/PlayfairDisplay-Regular.ttf"),
+    "playfairDisplay-bold": require("./assets/fonts/PlayfairDisplay-Bold.ttf"),
+  });
+
 export default function App() {
-  return <Home />;
+  const [fontsLoaded, setFontsLoaded] = React.useState(false);
+
+  if (fontsLoaded) {
+    return <Home />;
+  } else {
+    return (
+      <AppLoading
+        startAsync={getFonts}
+        onError={console.warn}
+        onFinish={() => setFontsLoaded(true)}
+      />
+    );
+  }
 }
