5a280c5262d2a16c8f18113728d6c0dff3c2586f
Hon <dennis.mejen@gmail.com>
2021-07-12 01:58:35 +0700

    [Added] 18 - Global Styles

--- screens/home.js
+++ screens/home.js
@@ -1,20 +1,12 @@
 import * as React from "react";
-import { StyleSheet, View, Text } from "react-native";
+import { View, Text } from "react-native";
+
+import { globalStyles } from "../styles/global";
 
 export default function Home() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.titleText}>Home Screen</Text>
+    <View style={globalStyles.container}>
+      <Text style={globalStyles.titleText}>Home Screen</Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    padding: 24,
-  },
-  titleText: {
-    fontFamily: "playfairDisplay-bold",
-    fontSize: 32,
-  },
-});
