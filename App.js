7bbb860d2294d8b63ad6cdd8af4bed36b4102bca
Hon <dennis.mejen@gmail.com>
2021-07-11 12:29:17 +0700

    Created a new Expo app

--- App.js
+++ App.js
@@ -1,66 +1,21 @@
-import * as React from "react";
-import {
-  StyleSheet,
-  View,
-  FlatList,
-  TouchableWithoutFeedback,
-  Keyboard,
-} from "react-native";
-
-import Header from "./components/header";
-import TodoItem from "./components/todoItem";
-import AddTodo from "./components/addTodo";
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  const [todos, setTodos] = React.useState([
-    { text: "buy coffee", key: "1" },
-    { text: "create an app", key: "2" },
-    { text: "play on the switch", key: "3" },
-  ]);
-
-  const pressHandler = (key) => {
-    setTodos((prevTodos) => {
-      return prevTodos.filter((todo) => todo.key !== key);
-    });
-  };
-
-  const submitHandler = (text) => {
-    setTodos((prevTodos) => {
-      return [{ text, key: Math.random().toString() }, ...prevTodos];
-    });
-  };
-
   return (
-    <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
-      <View style={styles.container}>
-        <Header />
-        <View style={styles.content}>
-          <AddTodo submitHandler={submitHandler} />
-          <View style={styles.list}>
-            <FlatList
-              data={todos}
-              renderItem={({ item }) => (
-                <TodoItem item={item} pressHandler={pressHandler} />
-              )}
-            />
-          </View>
-        </View>
-      </View>
-    </TouchableWithoutFeedback>
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-  },
-  content: {
-    flex: 1,
-    padding: 40,
-  },
-  list: {
-    flex: 1,
-    marginTop: 20,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
