45e1fc0655bb77b1006bd8f4ad9cb8179cc126c2
Hon <dennis.mejen@gmail.com>
2021-07-11 12:20:26 +0700

    [Added] 15 - Icons & More Flexbox

--- components/todoItem.js
+++ components/todoItem.js
@@ -1,10 +1,14 @@
 import * as React from "react";
-import { StyleSheet, Text, TouchableOpacity } from "react-native";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { EvilIcons } from "@expo/vector-icons";
 
 export default function TodoItem({ item, pressHandler }) {
   return (
     <TouchableOpacity onPress={() => pressHandler(item.key)}>
-      <Text style={styles.item}>{item.text}</Text>
+      <View style={styles.item}>
+        <EvilIcons name="trash" size={24} color="black" />
+        <Text style={styles.itemText}>{item.text}</Text>
+      </View>
     </TouchableOpacity>
   );
 }
@@ -15,7 +19,11 @@ const styles = StyleSheet.create({
     marginTop: 16,
     borderColor: "#bbb",
     borderWidth: 1,
-    borderStyle: "dashed",
     borderRadius: 10,
+    flexDirection: "row",
+    alignItems: "flex-end",
+  },
+  itemText: {
+    marginLeft: 8,
   },
 });
