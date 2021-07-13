b4cee2bddd4a2dd40a661402aadd7deae4fccfef
Hon <dennis.mejen@gmail.com>
2021-07-14 00:02:46 +0700

    [Added] 22 - Passing Data Between Screens

--- screens/home.js
+++ screens/home.js
@@ -1,17 +1,42 @@
 import * as React from "react";
-import { View, Text, Button } from "react-native";
+import { View, Text, FlatList, TouchableOpacity } from "react-native";
 
 import { globalStyles } from "../styles/global";
 
 export default function Home({ navigation }) {
-  const pressHandler = () => {
-    navigation.navigate("ReviewDetails");
-  };
+  const [reviews, setReviews] = React.useState([
+    {
+      title: "Zelda, Breath of Fresh Air",
+      rating: 5,
+      body: "lorem ipsum",
+      key: "1",
+    },
+    {
+      title: "Gotta Catch Them All (again)",
+      rating: 4,
+      body: "lorem ipsum",
+      key: "2",
+    },
+    {
+      title: "Not So Final Fantasy",
+      rating: 3,
+      body: "lorem ipsum",
+      key: "3",
+    },
+  ]);
 
   return (
     <View style={globalStyles.container}>
-      <Text style={globalStyles.titleText}>Home Screen</Text>
-      <Button title="go to review dets" onPress={pressHandler} />
+      <FlatList
+        data={reviews}
+        renderItem={({ item }) => (
+          <TouchableOpacity
+            onPress={() => navigation.navigate("ReviewDetails", item)}
+          >
+            <Text style={globalStyles.titleText}>{item.title}</Text>
+          </TouchableOpacity>
+        )}
+      />
     </View>
   );
 }
