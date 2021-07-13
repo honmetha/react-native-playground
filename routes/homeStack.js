53269f16a9cb80efc302d349e8658e35b387a41d
Hon <dennis.mejen@gmail.com>
2021-07-14 00:17:24 +0700

    [Added] 23 - Navigation Options

--- routes/homeStack.js
+++ routes/homeStack.js
@@ -7,12 +7,25 @@ import ReviewDetails from "../screens/reviewDetails";
 const screens = {
   Home: {
     screen: Home,
+    navigationOptions: {
+      title: "GameZone",
+      // headerStyle: { backgroundColor: "purple" },
+    },
   },
   ReviewDetails: {
     screen: ReviewDetails,
+    navigationOptions: {
+      title: "ReviewDetails",
+      // headerStyle: { backgroundColor: "purple" },
+    },
   },
 };
 
-const HomeStack = createStackNavigator(screens);
+const HomeStack = createStackNavigator(screens, {
+  defaultNavigationOptions: {
+    headerTintColor: "white",
+    headerStyle: { backgroundColor: "#006994", height: 80 },
+  },
+});
 
 export default createAppContainer(HomeStack);
--- styles/global.js
+++ styles/global.js
@@ -4,7 +4,7 @@ export const globalStyles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 20,
-    backgroundColor: "pink",
+    backgroundColor: "#ADD8E6",
   },
   titleText: {
     fontFamily: "playfairDisplay-bold",
